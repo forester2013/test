6fef80e0dbe0807eab8ef7de82cb5ee6d4f71dee
forester <forest@foresters-MacBook-Pro.local>
2016-12-23 18:39:14 -0800

    test-spark-server

--- spark/Server/src/main/java/Server.java
+++
@@ -1,8 +0,0 @@
-import static spark.Spark.*;
-
-public class Server {
-
-    public static void main(String[] args) {
-        get("/hello", (req, res) -> "Hello World!");
-    }
-}
--- spark/Test1/src/Server.java
+++
@@ -1,8 +0,0 @@
-/**
- * Created by forest on 12/23/16.
- */
-public class Server {
-    public static void main(String[] args) {
-
-    }
-}
